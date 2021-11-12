273e4b74442d52afe1ceadbb7272091302728d1f
Dmitry <43909412+DmitryRazumovskiy@users.noreply.github.com>
2021-11-12 15:26:01 +0300

    First Upload

---
+++ Scripts/Main/Manager/AudioManager.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    private GameManager gameManager;
+    private AudioData audioData;
+    private ObjectManager objectManager;
+    public bool allSound;
+    [SerializeField] private AudioSource[] exceptions;
+    List<AudioSource> audioSources;
+
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        objectManager = gameManager.ObjectManager_;
+    }
+
+    public void SetAudio(AudioData data)
+    {
+        audioData = data;
+
+        if (allSound && Application.isPlaying)
+        {
+            Collider[] gameObjects = FindObjectsOfType<Collider>();
+            foreach (Collider gO in gameObjects)
+            {
+                #region костыль для работы c ObjectAudioExperemental
+                if (gO.CompareTag("Audio Source Object"))  
+                {
+                    continue;
+                }
+                #endregion
+                CreateAudioSource(gO.transform);
+            }
+        }           
+
+
+        audioSources = FindObjectsOfType<AudioSource>().ToList();
+        
+        foreach (AudioSource audioSource in audioSources)
+        {
+            #region костыль для работы c ObjectAudioExperemental
+            if (audioSource.CompareTag("Audio Source"))
+            {
+                continue;
+            }
+            #endregion
+            SetAudioSourceSettings(audioSource);
+        }
+
+        for (int i = 0; i < exceptions.Length; i++)
+        {
+            audioSources.Remove(exceptions[i]);
+        }
+
+    }
+
+    public void CreateAudioSource(Transform AudioSourceObject)
+    {
+        if (AudioSourceObject.GetComponent<AudioSource>() == null)
+        {
+            AudioSource audioSource = AudioSourceObject.gameObject.AddComponent<AudioSource>();
+            audioSource.clip = audioData.basicSound;
+            audioSource.loop = true;
+            audioSource.spatialBlend = 1;
+        }
+    }
+
+    public void SetAudioSourceSettings(AudioSource audioSource)
+    {
+        InteractebleObject audioObject = audioSource.transform.GetComponent<InteractebleObject>();
+
+        audioSource.maxDistance = audioData.baseSoundDistance;
+        audioSource.rolloffMode = AudioRolloffMode.Custom;
+        if (!exceptions.Contains(audioSource))
+        {
+            audioSource.pitch = ScaleToFrequency(audioObject);
+            audioSource.volume = ScaleToVolume(audioObject);
+        }
+        audioSource.SetCustomCurve(AudioSourceCurveType.CustomRolloff, audioData.soundRollOff);
+    }
+
+    public void PlaySoundsOfCurrentLayer(int layer)
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            int objectLayer = audioSource.GetComponent<InteractebleObject>().GetLayer();
+            if (objectLayer == layer+1)
+            {
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.Stop();
+            }
+        }
+    }
+
+    public void MuteAll()
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.Stop();
+            //audioSource.volume = 0.1f;
+        }
+    }
+
+    public void PlaySoundsOfCurrentTargetChilds(int idOfTarget)
+    {        
+
+        Transform currentTarget = objectManager.FindTranformById(idOfTarget);
+        if (currentTarget == null)
+        {
+            print("Шеф! Все пропало! Все пропало!");
+            return;
+        }
+
+        MuteAll();
+
+        int childCount = currentTarget.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            #region костыль для работы c ObjectAudioExperemental
+            if (currentTarget.GetChild(i).CompareTag("Audio Source Object"))
+            {
+                currentTarget.GetChild(i).GetComponent<ObjectAudioExperemental>().PlaySound();
+            }
+            #endregion
+
+            if (currentTarget.GetChild(i).GetComponent<AudioSource>()!=null)
+            {                
+                
+                //currentTarget.GetChild(i).GetComponent<AudioSource>().volume = currentTarget.GetChild(i).GetComponent<InteractebleObject>().GetVolume();
+                currentTarget.GetChild(i).GetComponent<AudioSource>().Play();
+            }            
+        }
+    }    
+
+    public float ScaleToVolume(InteractebleObject interactebleObject)
+    {
+        float sizeModifier = interactebleObject.GetSizeModifier();
+        float volume = audioData.baseVolume * sizeModifier;
+
+        volume = Mathf.Clamp(volume, audioData.minVolume, audioData.maxVolume);
+        interactebleObject.SetVolume(volume);
+        return volume;
+    }
+
+
+    public float ScaleToFrequency(InteractebleObject interactebleObject)
+    {
+        float sizeModifier = interactebleObject.GetSizeModifier();
+        float frequency = audioData.maxFrequency - sizeModifier;
+        
+        frequency = Mathf.Clamp(frequency, audioData.minFrequency, audioData.maxFrequency);
+        return frequency;
+    }
+    
+}
---
+++ Scripts/Main/ObjectAudioExperemental.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAudioExperemental : MonoBehaviour
+{
+    [SerializeField] private AudioData audioData;
+    //[SerializeField] private AudioManager audioManager;
+    private RayCastController rayCastController;
+
+    AudioListener audioListener;
+    private AudioClip sound;
+    private float volume;
+    private GameObject audioSourceObject;
+    bool isTargeted;    
+
+    void Start()
+    {
+        rayCastController = FindObjectOfType<RayCastController>();
+        audioListener = FindObjectOfType<AudioListener>();
+        Initialization();
+    }
+
+    void LateUpdate()
+    {
+        AdjustAudioSource();
+    }
+
+    public void Initialization()
+    {
+        transform.tag = "Audio Source Object";
+        audioSourceObject = new GameObject("Audio Source");
+        audioSourceObject.transform.SetParent(transform);
+        audioSourceObject.transform.localPosition = Vector3.zero;
+        audioSourceObject.tag = "Audio Source";
+
+        CreateAudioSource(audioSourceObject.transform);
+        SetAudioSourceSettings(audioSourceObject.GetComponent<AudioSource>());        
+    }
+
+    void AdjustAudioSource()
+    {
+        isTargeted = rayCastController.GetLastSelectedTargetId() == transform.GetComponent<InteractebleObject>().GetId() ? true : false;
+        if (!isTargeted)
+        {
+            Bounds bounds = transform.GetComponent<MeshRenderer>().bounds;
+            Vector3 playerPosition = audioListener.transform.position;
+            Vector3 audioSourcePosition = playerPosition;
+
+            audioSourcePosition.x = Mathf.Clamp(audioSourcePosition.x, bounds.min.x, bounds.max.x);
+            audioSourcePosition.y = Mathf.Clamp(audioSourcePosition.y, bounds.min.y, bounds.max.y);
+            audioSourcePosition.z = Mathf.Clamp(audioSourcePosition.z, bounds.min.z, bounds.max.z);
+
+            audioSourceObject.transform.position = audioSourcePosition;
+        }
+    }
+
+    public void AdjustAudioSource(Vector3 rayHitPoint)    
+    {
+        audioSourceObject.transform.position = rayHitPoint;
+    }
+
+    #region после переноса в audioManager - удалить
+    private void CreateAudioSource(Transform AudioSourceObject)
+    {
+        if (AudioSourceObject.GetComponent<AudioSource>() == null)
+        {
+            AudioSource audioSource = AudioSourceObject.gameObject.AddComponent<AudioSource>();
+            
+            audioSource.clip = sound != null ? sound : audioData.basicSound;
+            audioSource.loop = true;
+            audioSource.spatialBlend = 1;
+        }
+    }
+    
+    public void SetAudioSourceSettings(AudioSource audioSource)
+    {
+        Transform parent = audioSource.transform.parent;
+
+        audioSource.maxDistance = audioData.baseSoundDistance;
+        audioSource.rolloffMode = AudioRolloffMode.Custom;        
+        audioSource.pitch = ScaleToFrequency(parent);
+        audioSource.volume = ScaleToVolume(parent);        
+        audioSource.SetCustomCurve(AudioSourceCurveType.CustomRolloff, audioData.soundRollOff);
+    }
+
+    public float ScaleToVolume(Transform currentObject)
+    {
+        float sizeModifier = (currentObject.localScale.x + currentObject.localScale.y + currentObject.localScale.z) / 3;
+        volume = audioData.baseVolume * sizeModifier;
+        volume = Mathf.Clamp(volume, audioData.minVolume, audioData.maxVolume);
+        return volume;
+    }
+
+    public float ScaleToFrequency(Transform currentObject)
+    {
+        float sizeModifier = (currentObject.localScale.x + currentObject.localScale.y + currentObject.localScale.z) / 3;
+        float frequency = audioData.maxFrequency - sizeModifier;
+
+        frequency = Mathf.Clamp(frequency, audioData.minFrequency, audioData.maxFrequency);
+        return frequency;
+    }
+    #endregion
+
+    public GameObject GetAudioSourceObject()
+    {
+        return audioSourceObject;
+    }
+
+    public void SetAudioSound(AudioClip audioClip)
+    {
+        sound = audioClip;
+    }
+
+    public void StopSound()
+    {
+        audioSourceObject.GetComponent<AudioSource>().Stop();
+    }
+
+    public void PlaySound()
+    {
+        audioSourceObject.GetComponent<AudioSource>().Play();
+    }
+
+    public void SetAudioData(AudioData data)
+    {
+        audioData = data;
+    }
+}
